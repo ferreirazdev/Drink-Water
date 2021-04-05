83e9db501ac16b71e3db1e57173a8fe9717b5973
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-04-05 15:26:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigCup()
+
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
 })
 
 function highlightCups(idx) {
+    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+
     smallCups.forEach((cup, idx2) => {
         if(idx2 <= idx) {
             cup.classList.add('full')
@@ -15,4 +21,19 @@ function highlightCups(idx) {
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+    }
 }
